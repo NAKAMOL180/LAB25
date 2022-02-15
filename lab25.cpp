463a75a17a18f3171ede3e71eb503d3e1fb758e6
Nakamol180 <nakamol_nuang@cmu.ac.th>
2022-02-15 17:27:05 +0700

    COMplete

--- lab25.cpp
+++ lab25.cpp
@@ -9,11 +9,14 @@ class ComplexNumber{
 		ComplexNumber(double,double);
 		ComplexNumber operator+(const ComplexNumber &);
 		ComplexNumber operator-(const ComplexNumber &);
+		ComplexNumber operator+(double);
+		ComplexNumber operator-(double);
 		ComplexNumber operator*(const ComplexNumber &);
 		ComplexNumber operator/(const ComplexNumber &);
 		bool operator==(const ComplexNumber &);
 		double abs();
 		double angle();
+		friend ostream & operator<<(ostream &s, const ComplexNumber &);
 };
 
 ComplexNumber::ComplexNumber(double x = 0,double y = 0){
@@ -28,7 +31,22 @@ ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 	return ComplexNumber(real-c.real,imag-c.imag);
 }
 
-//Write your code here
+ComplexNumber ComplexNumber::operator+(double num){
+	return ComplexNumber(real+num, image);
+}
+
+ComplexNumber ComplexNumber::operator-(double num){
+	return ComplexNumber(real-num, image);
+}
+
+ostream & operator<<(ostream &s, const ComplexNumber &c){
+	if(c.imag == 0)
+		return s << c.real;
+	else if(c.imag > 0)
+		return s << c.real << "+" << c.imag << "i" ;
+	else
+
+}
 
 int main(){
 	ComplexNumber a(1.5,2),b(3.2,-2.5),c(-1,1.2);	
